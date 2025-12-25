7e818381bcb28f49b5cecc89f85c3c9c2b54a33c
tailison-ramos <tailisonramos427@gmail.com>
2025-12-25 17:13:00 -0300

    fix: route name

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/index";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 
@@ -8,7 +8,6 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/about" element={<About />} />
-      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
